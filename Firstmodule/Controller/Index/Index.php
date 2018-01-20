61ad047fe1b1053c91a1ad0d0622f1ebfa2f93e8
Livia Baltatescu <livia@eadesign.ro>
2018-01-20 23:00:06 +0200

    First commit

---
+++ Firstmodule/Controller/Index/Index.php
@@ -0,0 +1,23 @@
+<?php
+namespace Livia\Firstmodule\Controller\Index;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $resultPageFactory;
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    )
+    {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+    public function execute()
+    {
+        echo "Livia-Firstmodule";
+        exit;
+    }
+}
